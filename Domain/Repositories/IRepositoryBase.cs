4aa5fdb93f849602585649d98bca9aec7f874618
Lucas Emanuel <lucaxemanuel@gmail.com>
2024-05-14 10:10:35 -0300

    isolate domain

---
+++ Domain/Repositories/IRepositoryBase.cs
@@ -0,0 +1,10 @@
+namespace Domain.Repositories;
+
+public interface IRepositoryBase<TEntity> where TEntity : class
+{
+    IEnumerable<TEntity> GetAll();
+    TEntity? GetById(string id);
+    void Insert(TEntity entity);
+    void Update(TEntity entity);
+    void Delete(TEntity entity);
+}
